57b3521d51e1294ead89fd91f1f34b1f0bba9116
hilmy057 <naufalh702@gmail.com>
2022-12-29 17:37:51 +0700

    first commit

---
+++ binarysearchtree_057/Program.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinarySearchTree_046
+{
+    class Node
+    {
+        public string info;
+        public Node leftchild;
+        public Node rightchild;
+
+        //Constructor for the NOde Class
+        public Node(string i, Node l, Node r)
+        {
+            info = i;
+            leftchild = l;
+            rightchild = r;
+        }
+    }
+    /* A node class consist of three things, the information, references to the right child, and references to the left child. */
+
+    class BinaryTree
+    {
+        public Node ROOT;
+        public BinaryTree()
+        {
+            ROOT = null; //initializing ROOT to null
+        }
+        public void insert(string element) //Insert a node in the binary sesarch tree
+        {
+            Node tmp, parent = null, currentNode = null;
+            search(element, ref parent, ref currentNode);
+            if (currentNode != null) // check if the node to be inserted already inserted or not
+            {
+                Console.WriteLine("Duplicate words not allowed");
+                return;
+            }
+            else // if the specified node is not present 
+            {
+                tmp = new Node(element, null, null); //creates a Node
+                if (parent == null) //if the trees is empty
+                {
+                    ROOT = tmp;
+                }
+                else if (string.Compare(element, parent.info) < 0)
+                {
+                    parent.leftchild = tmp;
+                }
+                else
+                {
+                    parent.rightchild = tmp;
+                }
+            }
+        }
+        public void search(string element, ref Node parent, ref Node currentNode)
+        {
+            //This function searchs the currentNode of the specified Node as well as the current Node of parent
+            currentNode = ROOT;
+            parent = null;
+            while ((currentNode != null) && (currentNode.info != element))
+            {
+                parent = currentNode;
+                if (string.Compare(element, currentNode.info) < 0)
+                    currentNode = currentNode.leftchild;
+                else
+                    currentNode = currentNode.rightchild;
+            }
+        }
+        public void inorder(Node ptr)
+        {
+            if (ROOT == null)
+            {
+                Console.WriteLine("Tree is empty");
+                return;
+            }
+            if (ptr != null)
+            {
+                inorder(ptr.leftchild);
+                Console.Write(ptr.info + " ");
+                inorder(ptr.rightchild);
+            }
+        }
+        public void preorder(Node ptr)
+        {
+            if (ROOT == null)
+            {
+                Console.WriteLine("Tree is Empty");
+                return;
+            }
+            if (ptr != null)
+            {
+                Console.WriteLine(ptr.info + " ");
+                preorder(ptr.leftchild);
+                preorder(ptr.rightchild);
+            }
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
